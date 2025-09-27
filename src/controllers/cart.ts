627ebca268bb8dc17af5b9e91ff4a699ca642eac
NISHTHA KHALASI <nishthakhalasi1@outlook.com>
2025-09-27 12:35:19 +0530

    fix: use process.env.PORT and add root route

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -7,7 +7,9 @@ import { ErrorCodes } from "../exceptions/root";
 import { UnauthorizedException } from "../exceptions/unauthorized";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2025-08-27.basil",
+});
 
 export const addItemToCart = async (req: Request, res: Response) => {
   if (!req.user) {
